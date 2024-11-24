3eadb6fe7821f4c0f5874e0ce66c9a754c4a4663
Maciej Blok <maciekblok@gmail.com>
2024-11-24 16:21:04 +0100

    Add synchronized map

---
+++ concurrency-and-multithreading/src/tasks/taskone/ThreadSafeMapSynchronized.java
@@ -0,0 +1,136 @@
+package tasks.taskone;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ThreadSafeMapSynchronized<K, V> implements Map {
+
+    private final List<Node<K, V>> map = new ArrayList();
+
+    public synchronized void printValues() {
+        map.forEach(System.out::println);
+    }
+
+    @Override
+    public synchronized int size() {
+        return map.size();
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return this.size() == 0;
+    }
+
+    @Override
+    public synchronized boolean containsKey(Object key) {
+        return getNodeByKey((K) key) != null;
+    }
+
+    private synchronized Node<K, V> getNodeByKey(K key) {
+        return map.stream().filter(n -> n.getKey().equals(key)).findFirst().orElse(null);
+    }
+
+    private synchronized Node<K, V> getNodeByValue(V value) {
+        return map.stream().filter(n -> n.getValue().equals(value)).findFirst().orElse(null);
+    }
+
+
+
+    @Override
+    public synchronized boolean containsValue(Object value) {
+        return getNodeByValue((V) value) != null;
+    }
+
+    @Override
+    public synchronized V get(Object key) {
+        Node<K, V> n;
+        return (n = getNodeByKey((K) key)) == null ? null : n.getValue();
+    }
+
+    @Override
+    public synchronized V put(Object key, Object value) {
+        var node = getNodeByKey((K) key);
+        if (node != null) {
+            V oldValue = node.getValue();
+            node.setValue((V) value);
+            return oldValue;
+        }
+        map.add(new Node<>((K) key, (V) value));
+        return null;
+    }
+
+    @Override
+    public synchronized V remove(Object key) {
+        var node = getNodeByKey((K) key);
+        if (node == null) return null;
+        map.remove(node);
+        return node.getValue();
+    }
+
+    @Override
+    public synchronized void putAll(Map m) {
+        m.entrySet().forEach(entry -> {
+            Map.Entry<?,?> e = (Map.Entry<?,?>) entry;
+            K key = (K) e.getKey();
+            V value = (V) e.getValue();
+            put(key, value);
+        });
+    }
+
+    @Override
+    public synchronized void clear() {
+        map.clear();
+    }
+
+    @Override
+    public synchronized Set<K> keySet() {
+        return map.stream().map(n -> n.getKey()).collect(Collectors.toSet());
+    }
+
+    @Override
+    public synchronized Collection<V> values() {
+        return map.stream().map(n -> n.getValue()).collect(Collectors.toList());
+    }
+
+    @Override
+    public synchronized Set<Map.Entry<K, V>> entrySet() {
+        return new HashSet<>(map);
+    }
+
+    private static class Node<K, V> implements Map.Entry<K, V> {
+
+        final K key;
+        V value;
+
+        Node(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        @Override
+        public final K getKey() {
+            return key;
+        }
+
+        @Override
+        public final V getValue() {
+            return value;
+        }
+
+        @Override
+        public V setValue(V newValue) {
+            V oldValue = value;
+            value = newValue;
+            return oldValue;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "key=" + key +
+                    ", value=" + value +
+                    '}';
+        }
+    }
+
+}
